8478f555bb8df1118be06b681ddd78a87c26cadf
Pavel Stovba <pro100zenb@gmail.com>
2023-10-21 15:23:26 +0300

    add new problems 605

---
+++ 605.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let counter = 0;
+    const newArr = flowerbed;
+    for(let i = 0; i < newArr.length; i++ )
+    {
+
+        if(newArr[i] === 0)
+
+        {
+            if((newArr[i - 1] === 0 || newArr[i - 1] === undefined) && (newArr[i + 1]=== 0  || newArr[i + 1]=== undefined))
+            {
+
+                newArr[i] = 1;
+                counter ++;
+
+            }
+        }
+
+    }
+    if (n <= counter)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
