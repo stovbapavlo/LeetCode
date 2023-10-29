b6480400c9c0e2197b48e715cceff96d00a7014a
Stovba Pavlo <stovbapavlo@gmail.com>
2023-10-29 10:07:39 +0300

    my solution 283 with console and method

---
+++ 283.js
@@ -0,0 +1,21 @@
+/*
+nums = [0,1,0,3,12]
+
+let zeroElements = []
+let noZeroElements = []
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0)
+    {
+        noZeroElements.push(nums[i]);
+    }
+    else
+    {
+        zeroElements.push(nums[i]);
+    }
+
+}
+let result = noZeroElements.concat(zeroElements);
+
+console.log(result);*/
+
