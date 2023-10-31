d4e032bc0cb534ae745a59205876795487dccaf2
Stovba Pavlo <stovbapavlo@gmail.com>
2023-10-31 11:15:53 +0200

    add problems 2433 leetcode version

--- 2433.js
+++ 2433.js
@@ -1,3 +1,4 @@
+/*
 let arr = [5 , 2 , 0 , 3 , 1]
 
 let newArr = [arr[0]];
@@ -9,4 +10,18 @@ for (let i = 1; i < arr.length; i++) {
     newArr.push(arr[i]^arr[i - 1]);
 
 }
-console.log(newArr);
+console.log(newArr);*/
+
+/**
+ * @param {number[]} pref
+ * @return {number[]}
+ */
+var findArray = function(pref) {
+    let result = [pref[0]];
+
+    for(i = 1; i < pref.length; i ++)
+    {
+        result.push(pref[i] ^ pref[i - 1])
+    }
+    return result;
+};
