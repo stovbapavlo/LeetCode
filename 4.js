44d7532455ea73464d4fea792283067e0364b21f
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-03 15:01:36 +0200

    add problems 4 finally

--- 4.js
+++ 4.js
@@ -1,3 +1,30 @@
+/*
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+
+    let arr = nums1.concat(nums2).sort((a , b) => a - b);
+    const n = arr.length;
+
+    if(n % 2 === 0){
+        const mid = n / 2;
+        return((arr[mid] + (arr[mid - 1])) / 2)
+    }
+    else{
+        const mid = Math.floor(n / 2);
+        return arr[mid];
+
+    }
+
+};
+
+
+
+
+
 let nums1 = [3, 2]
 let nums2 = [-2, -1]
 
@@ -18,4 +45,4 @@ else{
 
     console.log(arr[mid]);
 
-}
+}*/
