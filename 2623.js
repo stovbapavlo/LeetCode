5f950ab895ac258f7702c3d05eab28be6c4b5241
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-07 10:47:30 +0200

    update 2623

--- 2623.js
+++ 2623.js
@@ -16,11 +16,11 @@ function memoize(fn) {
 }
 
 
-let callCount = 0;
+/*let callCount = 0;
 const memoizedFn = memoize(function (a, b)
 {   callCount += 1;
     return a + b;
 })
 memoizedFn(2, 3) // 5
 memoizedFn(2, 3) // 5
-console.log(callCount) // 1
+console.log(callCount) // 1*/
