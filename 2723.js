4b01585e8e967da49ea3ee65e0480b3630fb2381
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-08 12:30:26 +0200

    add new problems solution 2723

--- 2723.js
+++ 2723.js
@@ -5,5 +5,9 @@
  */
 var addTwoPromises = async function(promise1, promise2) {
 
-    return await promise1 + await promise2;
+    //return await promise1 + await promise2;
+
+    const [value1 , value2] = await Promise.all([promise1, promise2])
+
+    return value1 + value2;
 };
