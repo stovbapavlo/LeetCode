bb22ab5651e4012c2226b7f7e509e386f4789b21
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-08 12:19:40 +0200

    add problems 2723 full solution

--- 2723.js
+++ 2723.js
@@ -1,2 +1,9 @@
-let promise1;
-let promise2;
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+
+    return await promise1 + await promise2;
+};
