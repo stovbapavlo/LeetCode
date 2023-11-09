f6cd676bfeca7ffe795df283daa271c077a962bf
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-09 09:24:22 +0200

    add problems 1759

---
+++ 1759.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countHomogenous = function(s) {
+    let left = 0;
+    let res = 0;
+    for(let right = 0; right < s.length; right++){
+        if(s[left] === s[right])
+        {
+            res += right - left + 1;
+        }
+        else{
+            res += 1;
+            left = right;
+        }
+
+    }
+    return res % (Math.pow(10, 9) + 7);
+};
