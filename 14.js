08df9585d55e24a037484086d9592915564d4154
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-12 19:12:29 +0200

    add problems 14

---
+++ 14.js
@@ -0,0 +1,11 @@
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0) return '';
+
+    for(let i = 0; i < strs[0].length; i++){
+        for(let s of strs){
+            if(s[i] !== strs[0][i]) return s.slice(0, i);
+        }
+    }
+    return strs[0];
+
+};
