d044ae927ecd34cb25cafb86d30f6072bda3c1fe
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-12 12:59:08 +0200

    add problems 48

---
+++ 48.js
@@ -0,0 +1,28 @@
+function rotateMatrix(matrix){
+    const n = matrix.length;
+
+    for(let i = 0; i < n; i++){
+        for(let j = i + 1; j < n; j++){
+            const temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+}
+const matrix = [
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16]
+];
+
+rotateMatrix(matrix);
+
+for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i].join(' '));
+
+}
+
