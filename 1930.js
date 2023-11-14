51eea315cee46d49e4fe46351e74c54e6659ed4b
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-14 10:23:49 +0200

    add problems 1930

--- 1930.js
+++ 1930.js
@@ -1,23 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countPalindromicSubsequence = function(s) {
+    let res = 0;
+    const uniq = new Set(s);
 
-let s = 'aabaca';
+    for(const c of uniq){
+        const start = s.indexOf(c);
+        const end = s.lastIndexOf(c);
 
-const uniq = new Set(s);
-
-let res = 0;
-console.log(uniq);
-
-for(const c of uniq){
-    const start = s.indexOf(c);
-    const end = s.lastIndexOf(c);
-
-    if(start < end){
-        res += new Set(s.slice(start + 1, end)).size;
-        console.log(res);
+        if(start < end){
+            res += new Set(s.slice(start + 1, end)).size;
+        }
     }
-
-}
-
-
-
-
-
+    return res;
+};
