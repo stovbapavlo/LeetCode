291343f5e9132bf14c478df72c92956ee193b803
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-15 10:15:38 +0200

    add problems 1846

---
+++ 1846.js
@@ -0,0 +1,17 @@
+let arr = [100, 1, 1000];
+
+const l = arr.length;
+
+const counter = new Array(l).fill(0);
+
+for(let i = 0; i < l; ++i) {
+    counter[Math.min(arr[i] - 1, l - 1)] += 1;
+}
+let ans = 1;
+
+for(let i = 1; i < l; ++i){
+
+    ans = Math.min(i + 1, ans + counter[i]);
+}
+
+console.log(ans);
