6bb1c3aebf655679a4c3cb0694ec22a0eaf448e8
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-15 10:17:20 +0200

    add problems 1846 leetcode versions

--- 1846.js
+++ 1846.js
@@ -1,17 +1,16 @@
-let arr = [100, 1, 1000];
+var maximumElementAfterDecrementingAndRearranging = function(arr) {
+    const l = arr.length;
+    const counter = new Array(l).fill(0);
 
-const l = arr.length;
+    for(let i = 0; i < l; ++i)
+    {
+        counter[Math.min(arr[i] - 1, l - 1)] += 1;
+    }
+    let ans = 1;
 
-const counter = new Array(l).fill(0);
+    for(let i = 1; i < l; ++i){
+        ans = Math.min(i + 1, ans + counter[i]);
+    }
+    return ans;
 
-for(let i = 0; i < l; ++i) {
-    counter[Math.min(arr[i] - 1, l - 1)] += 1;
-}
-let ans = 1;
-
-for(let i = 1; i < l; ++i){
-
-    ans = Math.min(i + 1, ans + counter[i]);
-}
-
-return ans;
+};
