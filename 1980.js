7c9179098e40ef5ba002748d58c23ce76feed8c7
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-16 09:25:00 +0200

    add problems 1980 leetcode versions

---
+++ 1980.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} nums
+ * @return {string}
+ */
+var findDifferentBinaryString = function(nums) {
+    let n = nums.length;
+
+    let result = '';
+
+    for(let i = 0; i < n; ++i)
+    {
+        result += (nums[i][i] === '0' ? '1' : '0');
+
+    }
+
+    return result;
+
+};
