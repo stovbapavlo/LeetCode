cb0408c5a5a8c2441bb052a2500a58fc6fa768f3
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-17 11:27:51 +0200

    appdate problems 1877

--- 1877.js
+++ 1877.js
@@ -1,12 +1,12 @@
-let arr = [3,5,4,2,4,6];
+let arr = [9,2,10,1,10,4,8,9,7,6,8,10,8,6,5,4,3,4,2,10];
 
-arr.sort();
+arr.sort((a, b)=> a - b);
 
 let res = [];
 
 for(let i = 0;i < arr.length / 2;  i++ )
 {
-    res += arr[i] + arr[arr.length - 1 - i]
+    res.push(arr[i] + arr[arr.length - 1 - i]);
 }
 
 
