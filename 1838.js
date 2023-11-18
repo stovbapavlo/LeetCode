e19d23800cf9f10a4a3960e0e77b545c7dcaa83b
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-18 11:22:29 +0200

    add problem 1838

--- 1838.js
+++ 1838.js
@@ -1,18 +1,22 @@
-let nums = [1,4,8,13], k = 5;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxFrequency = function(nums, k) {
+    nums.sort((a,b) => a - b);
+    let i = 0, j = 0;
+    let res = 0, total = 0;
 
-nums.sort((a,b) => a - b);
+    for(i = 0; i < nums.length; i++){
+        res += nums[i];
 
-let i = 0, j = 0;
-let res = 0, total = 0;
+        while((i - j + 1) * nums[i]- res > k){
+            res -= nums[j];
+            j++;
 
-for(i = 0; i < nums.length; i++){
-    res += nums[i];
-
-    while((i - j + 1) * nums[i]- res > k){
-        res -= nums[j];
-        j++;
-
+        }
+        total = Math.max(total, i - j + 1);
     }
-    total = Math.max(total, i - j + 1);
-}
-console.log(total);
+    return total;
+};
