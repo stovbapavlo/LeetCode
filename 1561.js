9aa970f9cdb7e7815e49649fb6e9fc8677f1f5df
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-24 12:57:16 +0200

    add problem 1561

--- 1561.js
+++ 1561.js
@@ -1,8 +1,39 @@
-piles.sort((a, b) => a - b);
-let res = 0;
+/**
+ * @param {number[]} piles
+ * @return {number}
+ */
+var maxCoins = function(piles) {
+    let max = 0;
+    let n = piles.length;
 
-for(let i = Math.floor(piles.length / 3); i < piles.length; i += 2){
-    res += piles[i];
-}
+    for(let i of piles){
+        if(max < i) max = i;
+    }
 
-return res;
+    let freq = new Array(max + 1).fill(0);
+
+    for(let i of piles){
+        freq[i]++;
+    }
+
+    let coins = 0;
+    let chance = Math.floor(n / 3);
+    let turn = 1;
+    let i = max;
+
+    while (chance !== 0) {
+        if (freq[i] > 0) {
+            if (turn === 1) turn = 0;
+            else {
+                chance--;
+                turn = 1;
+                coins += i;
+            }
+            freq[i]--;
+        } else {
+            i--;
+        }
+    }
+
+    return coins;
+};
