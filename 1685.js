90eadf50c804b1a5d41e632ac1bb7bc2d43b4c44
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-25 03:50:12 +0200

    add problem 1685 leetcode solution faster

--- 1685.js
+++ 1685.js
@@ -3,27 +3,15 @@
  * @return {number[]}
  */
 var getSumAbsoluteDifferences = function(nums) {
-    const n = nums.length;
+    let n = nums.length;
+    const result = new Array(n);
 
-    if (n < 2 || n > 10**5) {
-        return [];
-    }
+    result[0] = nums.reduce((a, b) => a + b, 0) - (n * nums[0])
 
-    const prefixSum = [0];
     for (let i = 1; i < n; i++) {
-        prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
-    }
-
-    const suffixSum = Array(n).fill(0);
-    for (let i = n - 2; i >= 0; i--) {
-        suffixSum[i] = suffixSum[i + 1] + nums[i + 1];
-    }
-
-    const result = [];
-
-    for (let i = 0; i < n; i++) {
-        result[i] = (i * nums[i] - prefixSum[i]) + (suffixSum[i] - (n - 1 - i) * nums[i]);
+        result[i] = result[i - 1] + (nums[i] - nums[i - 1]) * i - (nums[i] - nums[i - 1]) * (n - i);
     }
 
     return result;
+
 };
