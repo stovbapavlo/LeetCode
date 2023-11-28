86ea2726844e7b12ea0aac694ce8b7e16709c517
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-28 13:38:52 +0200

    add problem 2147 leetcode solution

---
+++ 2147.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} corridor
+ * @return {number}
+ */
+var numberOfWays = function(corridor) {
+    let m = 1e9 + 7;
+    let seat_number = [];
+    for(let i = 0; i < corridor.length; i++){
+        if(corridor[i] == 'S')
+            seat_number.push(i);
+    }
+    if(seat_number.length % 2 != 0 || seat_number.length == 0){
+        return 0;
+    }
+    let result = 1;
+    for(let i = 2; i < seat_number.length; i+= 2){
+        result = (result * (seat_number[i] - seat_number[i - 1])) % m
+    }
+    return result;
+};
