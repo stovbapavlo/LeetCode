f2dc8dea2d41fe17ebcad5a95bb77ff18ecbc479
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-30 02:11:46 +0200

    second solution 1611

--- 1611.js
+++ 1611.js
@@ -1,4 +1,5 @@
 
+/*
 var minimumOneBitOperations = function(n) {
     let answer = 0;
     let op = 1;
@@ -12,4 +13,16 @@ var minimumOneBitOperations = function(n) {
         bits--;
     }
     return answer;
+};*/
+
+let dp = { 0: 0 };
+var minimumOneBitOperations = function(n) {
+    if (!(n in dp)) {
+        let b = 1;
+        while ((b << 1) <= n) {
+            b = b << 1;
+        }
+        dp[n] = minimumOneBitOperations((b >> 1) ^ b ^ n) + 1 + b - 1;
+    }
+    return dp[n];
 };
