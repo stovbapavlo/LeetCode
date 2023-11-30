36be1749a6ca672dd0016c6413ca21222b51986e
Stovba Pavlo <stovbapavlo@gmail.com>
2023-11-30 02:09:23 +0200

    first solution 1611

---
+++ 1611.js
@@ -0,0 +1,15 @@
+
+var minimumOneBitOperations = function(n) {
+    let answer = 0;
+    let op = 1;
+    let bits = 30;
+    while(bits >= 0){
+        if(n & (1 << bits)){
+            let tmp = (1 << (bits + 1)) - 1;
+            answer += tmp * op;
+            op *= -1;
+        }
+        bits--;
+    }
+    return answer;
+};
