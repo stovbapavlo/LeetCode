ee734ca64be49c451b574c199ee8b428ff95bc3c
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-01 12:50:22 +0200

    second solution 1662

--- 1662.js
+++ 1662.js
@@ -1,3 +1,12 @@
-var arrayStringsAreEqual = function(word1, word2) {
-    return word1.join('') === word2.join('')
-};
+let left = "";
+let right = "";
+
+for (let i = 0; i < word1.length; i++) {
+    left += word1[i];
+}
+
+for (let i = 0; i < word2.length; i++) {
+    right += word2[i];
+}
+
+return left === right;
