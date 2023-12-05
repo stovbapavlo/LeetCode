ae37b0c69586e9bc92183a39948d2f9944a6ea16
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-05 12:12:39 +0200

    first solution 1688

---
+++ 1688.js
@@ -0,0 +1,15 @@
+let n = 7;
+let matches = 0;
+let teams = 0;
+
+while(n !== 1)
+{
+    matches += Math.floor(n / 2);
+
+    teams = Math.round(n / 2);
+    n = teams;
+    console.log(n);
+}
+console.log(matches);
+
+
