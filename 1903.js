83346b76b08300750ce81f7dda80001b09b4a503
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-07 12:10:24 +0200

    last solution  1903

--- 1903.js
+++ 1903.js
@@ -1,7 +1,7 @@
 var largestOddNumber = function(num) {
     for (let i = num.length - 1; i >= 0; i--) {
         if (num[i] % 2 === 1) {
-            return num.slice(0, i + 1);;
+            return num.slice(0, i + 1);
         }
     }
     return '';
