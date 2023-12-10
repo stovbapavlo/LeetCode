4e019f1f48a2c44f96a3e90678ac58c72f9b3e82
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-10 03:17:20 +0200

    last  solution 867

--- 867.js
+++ 867.js
@@ -1,18 +1,14 @@
-let matrix = [[1,2,3],[4,5,6],[7,8,9]];
-let n = matrix[1].length;
-let res = [];
-let arr = [];
+const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const rows = matrix.length;
+const cols = matrix[0].length;
+const result = [];
 
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        arr.push(matrix[j][i]);
-
+for (let i = 0; i < cols; i++) {
+    const column = [];
+    for (const row of matrix) {
+        column.push(row[i]);
     }
-    res.push(arr);
-    arr = [];
-
-
+    result.push(column);
 }
 
-
-console.log(res);
+console.log(result);
