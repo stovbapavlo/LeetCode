43580444d4bfce7fd669ddfe1a36399bf62f73da
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-14 11:51:57 +0200

    second solution 2482

--- 2482.js
+++ 2482.js
@@ -5,22 +5,23 @@ let n = grid[0].length;
 const diff = Array.from({length: m}, () => Array(n).fill(0));
 
 const oneRow = Array(m).fill(0);
-const zeroRow = Array(m).fill(0);
+//const zeroRow = Array(m).fill(0);
 const oneCol = Array(n).fill(0);
-const zeroCol = Array(n).fill(0);
+//const zeroCol = Array(n).fill(0);
 
 for(let i = 0; i < m; i++){
     for(let j = 0; j < n; j++){
 
         oneRow[i] += grid[i][j];
         oneCol[j] += grid[i][j];
-        zeroRow[i] += 1 - grid[i][j];
-        zeroCol[j] += 1 - grid[i][j];
+        //zeroRow[i] += 1 - grid[i][j];
+        //zeroCol[j] += 1 - grid[i][j];
     }
 }
 
 for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-        diff[i][j] = oneRow[i] + oneCol[j] - zeroRow[i] - zeroCol[j];
+        diff[i][j] = 2 * (oneRow[i] + oneCol[j]) - m - n;
     }
 }
+
