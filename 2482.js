67473ad59dc6eab687a27c4ca0950958ec214e43
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-14 11:58:20 +0200

    solution 2482

--- 2482.js
+++ 2482.js
@@ -25,3 +25,26 @@ for (let i = 0; i < m; i++) {
     }
 }
 
+/*
+let row = Array(grid.length).fill(0);
+     let column = Array(grid[0].length).fill(0);
+    for(let i = 0 ; i < grid.length; i++){
+        for(let j = 0 ; j < grid[0].length; j++){
+            let value = grid[i][j];
+            row[i]+=value;
+            column[j]+=value;
+        }
+    }
+    let m = grid.length;
+    let n = grid[0].length;
+      for(let i = 0 ; i < grid.length; i++){
+
+        for(let j = 0 ; j < grid[0].length; j++){
+
+            grid[i][j] = row[i] - (m - row[i]) + column[j] - (n- column[j]);
+        }
+    }
+    return grid;
+
+*/
+
