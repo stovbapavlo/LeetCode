1b2c3571ed8f86dc2636fc403f6c9c295a84ce9b
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-14 12:17:56 +0200

    solution 231

--- 231.js
+++ 231.js
@@ -3,9 +3,25 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if(n <= 0) return false;
+   /* if(n <= 0) return false;
 
-    return (Math.log(n) / Math.log(2)).toFixed(10) % 1 === 0;
+    return (Math.log(n) / Math.log(2)).toFixed(10) % 1 === 0;*/
+
+    if(n === 1 || n === 2) return true;
+
+    let value = 1;
+
+    for(let i = 0; i <= n; i += n) {
+        if(i === n) {
+            for(let j = 1; j <= i; j *= 2) {
+                value = j;
+            }
+
+            break;
+        }
+    }
+
+    return value === n ? true : false;
 };
 
 //solution for bitary trick
