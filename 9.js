36077467b0f3ddf442310af5f45117ab4c99dd3c
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-14 12:20:19 +0200

    solution 9

--- 9.js
+++ 9.js
@@ -1,4 +1,13 @@
 let n = 1234;
 var isPalindrome = function(x) {
-    return x === parseInt(x.toString().split('').reverse().join(''))
+    //return x === parseInt(x.toString().split('').reverse().join(''))
+
+    let _x = x;
+    let reverse = 0;
+    while (_x > 0) {
+        const mod = _x % 10;
+        reverse = reverse * 10 + mod;
+        _x = Math.floor(_x / 10);
+    }
+    return reverse === x;
 };
