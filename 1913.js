f41be5012d5aeea11f418a0fe7bc08cdcac796c9
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-18 04:05:50 +0200

    leetcode solution 1913

--- 1913.js
+++ 1913.js
@@ -1,4 +1,21 @@
+/*
 nums.sort((a,b) => a - b);
 let n = nums.length;
+*/
 
 //return (nums[n - 1] * nums[n -2]) - (nums[0] * nums[1]);
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function(nums) {
+    let min0=Math.min(...nums)
+    let maxN=Math.max(...nums)
+    nums.splice(nums.indexOf(min0),1)
+    nums.splice(nums.indexOf(maxN),1)
+    let min1=Math.min(...nums)
+    let maxM=Math.max(...nums)
+
+    return maxM*maxN - min0*min1
+};
