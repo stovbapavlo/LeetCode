a0c70ae1625d57b469175edc8f3d69552814a37a
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-22 10:37:22 +0200

    solution 1422

--- 1422.js
+++ 1422.js
@@ -4,7 +4,7 @@ let res = 0;
 let right = s.split("1").length - 1;
 let left = 0;
 
-for(let i = 0; i < s.length; i++){
+for(let i = 0; i < s.length - 1; i++){
     left += s[i] === '0'? 1 : 0;
     right -= s[i] === '1'? 1 : 0;
 
