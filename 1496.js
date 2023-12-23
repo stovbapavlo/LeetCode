3c30119cc6a29893e197e85b5c95703296f87512
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-23 11:46:22 +0200

    solution 1496

--- 1496.js
+++ 1496.js
@@ -61,3 +61,28 @@ console.log(isPathCrossing(path));
     }
 
     return false;*/
+
+
+/*const positions = new Set()
+    let x = 0;
+    let y = 0;
+    const navigate = {
+        "N": { x: 0, y: +1 },
+        "S": { x: 0, y: -1 },
+        "W": { x: -1, y: 0 },
+        "E": { x: +1, y: 0 },
+    }
+    positions.add("0-0")
+    for (const direction of path) {
+        x += navigate[direction].x;
+        y += navigate[direction].y;
+
+        if (positions.has(`${x}-${y}`)) {
+            console.log(positions);
+            return true
+        }
+
+        positions.add(`${x}-${y}`);
+    }
+
+    return false;*/
