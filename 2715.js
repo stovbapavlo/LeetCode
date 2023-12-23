3d388a8593e112b281a20409cdf6bb440e10b3ef
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-23 11:56:16 +0200

    solution 2715

---
+++ 2715.js
@@ -0,0 +1,10 @@
+var cancellable = function(fn, args, t) {
+    const cancelFn = function(){
+        clearTimeout(timer);
+    }
+    const timer = setTimeout(() => {
+        fn(...args)
+    }, t);
+    return cancelFn;
+
+};
