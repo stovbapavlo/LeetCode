f2d8c6286f68574517abd99381209c3906848e24
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-25 12:13:28 +0200

    solution 91

--- 91.js
+++ 91.js
@@ -3,6 +3,20 @@
  * @return {number}
  */
 var numDecodings = function(s) {
+    const dp = new Array(s.length +1).fill(0);
+    dp[s.length] = 1;
+
+    for(let i = s.length -1; i >= 0 ; i--){
+        if(s[i] == 0) continue;
+        dp[i]  = dp[i+1];
+        if(s[i] + s[i+1] <= 26) dp[i] += dp[i+2];
+    }
+
+    return dp[0];
+};
+
+/*
+var numDecodings = function(s) {
     if(s == null || s.length === 0) return 0;
     if(s[0] === '0') return 0;
 
@@ -23,4 +37,4 @@ var numDecodings = function(s) {
         }
     }
     return dp[s.length];
-};
+};*/
