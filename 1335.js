c90a2c0237481196f502ee1576d0c0a9979adf2c
Stovba Pavlo <stovbapavlo@gmail.com>
2023-12-29 11:03:33 +0200

    solution 1335

--- 1335.js
+++ 1335.js
@@ -1,29 +1,22 @@
-var minDifficulty = function(jobDifficulty, d) {
-    if(jobDifficulty.length < d) return -1;
+/**
+ * @param {number[]} jobDifficulty
+ * @param {number} d
+ * @return {number}
+ */
+var minDifficulty = function(jobs, d) {
+    const n = jobs.length;
+    if (n < d) return -1;
+    const dp = [...new Array(n + 1)].map(() => new Array(d + 1).fill(Number.MAX_SAFE_INTEGER));
+    dp[0][0] = 0;
 
-    const cache = {};
-
-    const dfs = (start, numDays) => {
-        if(numDays === d){
-            return start === jobDifficulty.length ? 0 : Infinity;
+    for (let i = 1; i <= n; ++i) {
+        for (let k = 1; k <= d; ++k) {
+            let max = 0;
+            for (let j = i - 1; j >= k - 1; --j) {
+                max = Math.max(max, jobs[j + 1 - 1]);
+                dp[i][k] = Math.min(dp[j][k-1] + max, dp[i][k]);
+            }
         }
-
-        const key =  `${start}-${numDays}`;
-
-        if(cache[key] !== undefined) return cache[key];
-
-        const end = jobDifficulty.length - d + numDays;
-
-        let result = Infinity;
-
-        let max = -Infinity;
-
-        for(let i = start; i <= end; i++){
-            max = Math.max(max, jobDifficulty[i]);
-            result = Math.min(result, max + dfs(i + 1, numDays + 1));
-        }
-
-        return cache[key] = result;
     }
-    return dfs(0, 0);
+    return dp[n][d];
 };
