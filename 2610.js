1afd9af8b27aa292e051de99d9f0eb8006e1a783
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-02 13:37:48 +0200

    solution 2610

---
+++ 2610.js
@@ -0,0 +1,13 @@
+let output = [];
+let map = new Map();
+
+for(let num of nums){
+    let idx = map.get(num) || 0;
+    let arr = output[idx] || [];
+
+    arr.push(num);
+    output[idx] = arr;
+    map.set(num, idx + 1);
+}
+
+return output;
