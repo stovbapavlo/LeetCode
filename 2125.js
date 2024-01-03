c830d54e105f6f1c4d3f1fe032756e087a5d3100
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-03 11:04:09 +0200

    solution 2125

--- 2125.js
+++ 2125.js
@@ -3,15 +3,14 @@ let bank = ["011001","000000","010100","001000"];
 let zeroCounter = 0;
 let res = 0
 
-for(let i = 0; i < bank.length - 1; i++){
-    for(let j = 0; j < bank[0].length; j++){
-        if (bank[i][j] === '1' && bank[i + 1][j] === '1'){
-            for(let k = i + 1; k < bank.length; k++){
-                if(bank[k][j] === "1"){
-                    res++;
-                }
-            }
+for(let i = 0; i < bank.length; i++){
+    let count = 0;
+    for(let j = 0; j < bank[i].length; j++){
+        if(bank[i][j] === '1'){
+            res += zeroCounter;
+            count++;
         }
     }
+    zeroCounter = count ? count: zeroCounter;
 }
 console.log(res)
