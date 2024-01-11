b2b4f23d3fb5c26d0434808c716cf34aa2e84dfb
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-11 13:36:00 +0200

    solution 1026

--- 1026.js
+++ 1026.js
@@ -11,3 +11,22 @@ var maxAncestorDiff = function(root) {
 
     return dfs(root, -Infinity, +Infinity);
 };
+
+/*var maxAncestorDiff = function(root) {
+    let diff = 0;
+    if(!root) return 0;
+    const stack = [[root, root.val, root.val]];
+    while(stack.length > 0){
+        let [node, currMax, currMin] = stack.pop();
+        currMax = Math.max(currMax, node.val);
+        currMin = Math.min(currMin, node.val);
+        diff = Math.max(diff, currMax - currMin);
+        if(node.right !== null){
+            stack.push([node.right, currMax, currMin]);
+        }
+        if(node.left !== null){
+            stack.push([node.left, currMax, currMin]);
+        }
+    }
+    return diff;
+};*/
