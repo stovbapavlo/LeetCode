aeda7cf0874eb9554e36cb2543052efc2c0e5ae1
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-12 20:35:06 +0200

    solution 1704

--- 1704.js
+++ 1704.js
@@ -4,8 +4,10 @@
  */
 var halvesAreAlike = function(s) {
 
-    let a = s.slice(0,s.length/2), b = s.slice(s.length/2);
-    let count = 0, vowels = "aeiouAEIOU";
+    let a = s.slice(0,s.length/2)
+    let b = s.slice(s.length/2);
+    let count = 0
+    let vowels = "aeiouAEIOU";
 
     for (let i = 0; i < a.length; i++) {
         if (vowels.includes(a[i])) count++;
@@ -25,7 +27,7 @@ let b = s.slice(s.length / 2, s.length);
 let counterA = 0;
 let counterB = 0;
 
-for(const words of vowels){
+for(const words of vowels)
     for(const char of a){
         if(char === words){
             counterA++;
