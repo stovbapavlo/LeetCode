a779009611f5a9d3a40dcb0943cfdb2828c85bdd
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-13 14:45:29 +0200

    solution 1347

--- 1347.js
+++ 1347.js
@@ -1,18 +1,27 @@
-let s = "bab";
-let t = "aba";
-
-let setS = new Set(s);
-let setT = new Set(t);
-let sortS = s.split('').sort().join('');
-let sortT = t.split('').sort().join('');
-
-let counter = 0;
-
-console.log(setS);
-console.log(setT);
-
-for(let i = 0; i < s.length; i++){
-    if(sortT[i] !== sortS[i]){
-        counter++;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function(s, t) {
+    let hasMap = {};
+    for(let letter of s){
+        if(hasMap[letter]){
+            hasMap[letter]++;
+        }
+        else{
+            hasMap[letter] = 1;
+        }
     }
-}
+    let changes = 0;
+    for(let letter of t){
+        if(hasMap[letter]){
+            hasMap[letter] --;
+        }
+        else{
+            changes++;
+        }
+    }
+    return changes;
+
+};
