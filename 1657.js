461a5de5209d82cdb0a5fd7cc6ca8f2795e95327
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-14 14:10:56 +0200

    solution 1657

--- 1657.js
+++ 1657.js
@@ -1,22 +1,15 @@
+var closeStrings = function(word1, word2) {
+    if (word1.length !== word2.length) return false;
+    const obj1 = {};
+    const obj2 = {};
 
-
-function swap(str, index1, index2){
-    let strArray = str.split('');
-    let temp = strArray[index1];
-    strArray[index1] = strArray[index2];
-    strArray[index2] = temp;
-
-    return strArray.join('');
-
-}
-function transform(str, char1, char2){
-    let strArray = str.split('');
-    for (let i in strArray) {
-        if (strArray[i] === char1) {
-            strArray[i] = char2;
-        } else if (strArray[i] === char2) {
-            strArray[i] = char1;
-        }
+    for (let i = 0; i < word1.length; i++) {
+        obj1[word1[i]] = obj1[word1[i]] ? obj1[word1[i]] + 1 : 1;
+        obj2[word2[i]] = obj2[word2[i]] ? obj2[word2[i]] + 1 : 1;
     }
-    return strArray.join('');
-}
+
+    const letters = Object.keys(obj1).sort().join("") === Object.keys(obj2).sort().join("");
+    const values = Object.values(obj1).sort().join("") === Object.values(obj2).sort().join("");
+
+    return letters && values;
+};
