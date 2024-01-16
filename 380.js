f88f5cc758c88f79f692b380e57288ca7dc676a8
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-16 13:06:26 +0200

    solution 380

--- 380.js
+++ 380.js
@@ -16,15 +16,7 @@
  * @return {number}
  */
 
-
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * var obj = new RandomizedSet()
- * var param_1 = obj.insert(val)
- * var param_2 = obj.remove(val)
- * var param_3 = obj.getRandom()
- */class RandomizedSet {
+ class RandomizedSet {
     constructor() {
         this.list = [];
         this.map = new Map();
