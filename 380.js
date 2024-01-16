d387fe5d3fbb8cd02a70d03c33075495ce30c7ff
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-16 13:05:46 +0200

    solution 380

--- 380.js
+++ 380.js
@@ -45,11 +45,3 @@ RandomizedSet.prototype.getRandom = function() {
     const randomIndex = Math.floor(Math.random() * this.list.length);
     return this.list[randomIndex];
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * var obj = new RandomizedSet()
- * var param_1 = obj.insert(val)
- * var param_2 = obj.remove(val)
- * var param_3 = obj.getRandom()
- */
