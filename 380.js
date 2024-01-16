fced510b1f47d55a27d5d2a631ac697827094476
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-16 13:06:07 +0200

    solution 380

--- 380.js
+++ 380.js
@@ -1,47 +1,60 @@
 
-var RandomizedSet = function() {
-    this.list = [];
-    this.map = new Map();
-};
 
 /**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.search = function(val) {
-    return this.map.has(val);
-};
-/**
- * @param {number} val
- * @return {boolean}
- */
-RandomizedSet.prototype.insert = function(val) {
-    if (this.search(val)) return false;
 
-    this.list.push(val);
-    this.map.set(val, this.list.length - 1);
-    return true;
-};
 
 /**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.remove = function(val) {
-    if (!this.search(val)) return false;
 
-    const index = this.map.get(val);
-    this.list[index] = this.list[this.list.length - 1];
-    this.map.set(this.list[index], index);
-    this.list.pop();
-    this.map.delete(val);
-    return true;
-};
 
 /**
  * @return {number}
  */
-RandomizedSet.prototype.getRandom = function() {
-    const randomIndex = Math.floor(Math.random() * this.list.length);
-    return this.list[randomIndex];
-};
+
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */class RandomizedSet {
+    constructor() {
+        this.list = [];
+        this.map = new Map();
+    }
+
+    search(val) {
+        return this.map.has(val);
+    }
+
+    insert(val) {
+        if (this.search(val)) return false;
+
+        this.list.push(val);
+        this.map.set(val, this.list.length - 1);
+        return true;
+    }
+
+    remove(val) {
+        if (!this.search(val)) return false;
+
+        const index = this.map.get(val);
+        this.list[index] = this.list[this.list.length - 1];
+        this.map.set(this.list[index], index);
+        this.list.pop();
+        this.map.delete(val);
+        return true;
+    }
+
+    getRandom() {
+        const randomIndex = Math.floor(Math.random() * this.list.length);
+        return this.list[randomIndex];
+    }
+}
