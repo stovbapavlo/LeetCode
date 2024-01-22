3822b5988ed4ebf64c1de94ee3bfde2c0ad4f27f
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-22 14:33:05 +0200

    solution 645

--- 645.js
+++ 645.js
@@ -1,38 +1,25 @@
-/*
-let arr = new Array(nums.length).fill(0);
-let res = [];
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    let arr = new Array(nums.length).fill(0);
+    let res = [];
 
-for (let i = 0; i < nums.length; i++) {
-    if (arr[nums[i] - 1] === 0) {
-        arr[nums[i] - 1] = nums[i];
-    } else {
-        res.push(nums[i]);
+    for (let i = 0; i < nums.length; i++) {
+        if (arr[nums[i] - 1] === 0) {
+            arr[nums[i] - 1] = nums[i];
+        } else {
+            res.push(nums[i]);
+        }
     }
-}
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
-        res.push(i + 1);
-        break;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            res.push(i + 1);
+            break;
+        }
     }
-}
 
-return res;let arr = new Array(nums.length).fill(0);
-let res = [];
-
-for (let i = 0; i < nums.length; i++) {
-    if (arr[nums[i] - 1] === 0) {
-        arr[nums[i] - 1] = nums[i];
-    } else {
-        res.push(nums[i]);
-    }
-}
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
-        res.push(i + 1);
-        break;
-    }
-}
-
-return res;*/
+    return res;
+};
