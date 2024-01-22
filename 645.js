cfa640f263582ad76129a16074d5166e79aebf9b
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-22 14:32:49 +0200

    solution 645

--- 645.js
+++ 645.js
@@ -1,26 +1,38 @@
 /*
-let dup = -1, missing = -1;
+let arr = new Array(nums.length).fill(0);
+let res = [];
 
-for (let i = 1; i <= nums.length; i++) {
-    let count = nums.filter(num => num === i).length;
-    if (count === 2) {
-        dup = i;
-    } else if (count === 0) {
-        missing = i;
+for (let i = 0; i < nums.length; i++) {
+    if (arr[nums[i] - 1] === 0) {
+        arr[nums[i] - 1] = nums[i];
+    } else {
+        res.push(nums[i]);
     }
 }
 
-return [dup, missing];
-let dup = -1, missing = -1;
-
-for (let i = 1; i <= nums.length; i++) {
-    let count = nums.filter(num => num === i).length;
-    if (count === 2) {
-        dup = i;
-    } else if (count === 0) {
-        missing = i;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+        res.push(i + 1);
+        break;
     }
 }
 
-return [dup, missing];
-*/
+return res;let arr = new Array(nums.length).fill(0);
+let res = [];
+
+for (let i = 0; i < nums.length; i++) {
+    if (arr[nums[i] - 1] === 0) {
+        arr[nums[i] - 1] = nums[i];
+    } else {
+        res.push(nums[i]);
+    }
+}
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+        res.push(i + 1);
+        break;
+    }
+}
+
+return res;*/
