8eab5a9825aac3b437ef991805bbbaaaca0b3604
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-23 15:04:29 +0200

    solution 1249

--- 1249.js
+++ 1249.js
@@ -25,3 +25,23 @@ function generateSubsequences(arr, current, index){
 let arr = ["un","iq","ue"];
 
 
+/*
+
+/!**
+ * @param {string[]} arr
+ * @return {number}
+ *!/
+var maxLength = function(arr) {
+    let res = 0;
+    function solve(index, cur) {
+        if(cur.length !== new Set(cur.split('')).size) {
+            return;
+        }
+        res = Math.max(res, cur.length);
+        for(let i = index; i < arr.length; i++) {
+            solve(i + 1, `${cur}${arr[i]}`);
+        }
+    }
+    solve(0, '');
+    return res;
+};*/
