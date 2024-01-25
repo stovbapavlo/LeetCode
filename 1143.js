c91c7d29f037bd9555cc251e8769c3ceea222508
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-25 15:00:34 +0200

    solution 1457

--- 1143.js
+++ 1143.js
@@ -1,16 +1,23 @@
-let text1 = "abcde", text2 = "ace"
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+    const m = text1.length;
+    const n = text2.length;
 
-let res = '';
+    const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
 
-for (let i = 0; i < text1.length; i++) {
-    for (let j = 0; j < text2.length; j++) {
-        if(text1[i] === text2[j]){
-            res += text1[i] ;
-            break;
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
-
     }
 
-}
-
-console.log(res.length)
+    return dp[m][n];
+};
