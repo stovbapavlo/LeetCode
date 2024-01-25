21746fac9eeba2cc2db7210fc76ce006ea4f773a
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-25 15:01:44 +0200

    solution 1457

--- 1143.js
+++ 1143.js
@@ -4,20 +4,23 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    const m = text1.length;
-    const n = text2.length;
-
-    const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
-
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (text1[i - 1] === text2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
+    const n1 = text1.length;
+    const n2 = text2.length;
+    let prev = Array(n2+1).fill(0);
+    let curr = Array(n2+1).fill(0);
+    for(let i=0; i<=n2; i++){
+        prev[i] = 0;
     }
 
-    return dp[m][n];
+    for(let i=1; i<=n1; i++){
+        for(let j=1; j<=n2; j++){
+            if(text1.charAt(i-1) === text2.charAt(j-1)){
+                curr[j] = 1 + prev[j-1];
+            }else{
+                curr[j] = Math.max(prev[j], curr[j-1]);
+            }
+        }
+        prev = [...curr];
+    }
+    return prev[n2];
 };
