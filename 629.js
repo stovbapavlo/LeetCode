b6f444418c7e2384d2575cf7388c96fbae5ef748
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-27 13:38:35 +0200

    solution 629

--- 629.js
+++ 629.js
@@ -1,19 +1,20 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
 var kInversePairs = function(n, k) {
-    const m = 10**9 + 7;
-    let dp = new Array(1001).fill(0).map(() => new Array(1001).fill(0));
-    dp[0][0] = 1;
-
-    for(let i = 1; i <= n; i++) {
-        for(let j = 0; j <= k; j++) {
-            for(let x = 0; x <= Math.min(j, i -1); x++) {
-                if(j - x >= 0) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % m;
-                }
-            }
+    let mod = 1e9 + 7;
+    let dp = Array(k + 1).fill(0);
+    for (let i = 0; i <= n; ++i) {
+        let temp = Array(k + 1).fill(0);
+        temp[0] = 1;
+        for (let j = 1; j <= k; ++j) {
+            let val = (dp[j] + mod - ((j - i) >= 0 ? dp[j - i] : 0)) % mod;
+            temp[j] = (temp[j - 1] + val) % mod;
         }
-
+        dp = temp;
     }
-
-    return dp[n][k];
+    return ((dp[k] + mod - (k > 0 ? dp[k - 1] : 0)) % mod);
 
 };
