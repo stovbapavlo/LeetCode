89fc1e2461c050d46d28654b69f28e9fbc1f10a0
Stovba Pavlo <stovbapavlo@gmail.com>
2024-01-31 13:20:55 +0200

    solution 739

--- 739.js
+++ 739.js
@@ -1,10 +1,8 @@
     const temperatures = [73,74,75,71,69,72,76,73];
 
     let res = [];
-
+    let counter = 0;
     for(let i = 0; i < temperatures.length; i++){
-        let daysToWait = 0;
-        let foundWarmerdDay = false;
         for(let j = i + 1; j < temperatures.length; j++){
             console.log(temperatures[j])
             if(temperatures[i] < temperatures[j]){
