abe72842186a33da1720c6c88573edab949904f9
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-01 15:03:23 +0200

    solution 2966

--- 2966.js
+++ 2966.js
@@ -4,16 +4,15 @@
  * @return {number[][]}
  */
 var divideArray = function(nums, k) {
-    nums.sort((a, b) => a - b);
+    const n = nums.length;
     const res = [];
-
-    for (let i = 0; i < nums.length; i+=3){
-        if (nums[i+1] - nums[i] <= k && nums[i+2] - nums[i] <= k){
-            res.push([nums[i], nums[i+1], nums[i+2]])
-        } else {
-            return []
+    nums.sort((a,b)=>a-b);
+    for(let i = 0; i<n; i++) {
+        if((nums[i+1] - nums[i] > k) || (nums[i+2] - nums[i+1] > k) || (nums[i+2] - nums[i] > k)){
+            return [];
         }
+        res.push([nums[i], nums[i+1], nums[i+2]]);
+        i += 2;
     }
-
     return res;
 };
