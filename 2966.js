8e25b5704d15ac504011f38f27ba5de148f46e02
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-01 14:58:27 +0200

    solution 2966

--- 2966.js
+++ 2966.js
@@ -1,6 +1,6 @@
 let nums = [1,3,4,8,7,9,3,5,1], k = 2
 
-let res = []
+const res = []
 
 nums.sort((a, b) => a - b);
 
@@ -8,7 +8,7 @@ console.log(nums);
 
 for (let i = 2; i < nums.length; i = i + 3) {
     if((nums[i -1] - nums[i - 2] > k) || (nums[i] - nums[i - 1] > k) || (nums[i] - nums[i - 2] > k)) {
-        return [];
+       // return [];
     }
     else {
         res.push([nums[i - 2], nums[i - 1], nums[i]])
