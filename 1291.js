e0ee0bee81e1fe2e3b13f58995942cd588578451
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-02 15:18:42 +0200

    solution 1291

--- 1291.js
+++ 1291.js
@@ -1,37 +1,22 @@
-const low = 100, high = 300;
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+var sequentialDigits = function(low, high) {
+    let result = [];
+    let sample = '123456789';
+    let n = 10;
 
-let res = [];
+    let lowLength = low.toString().length;
+    let highLength = high.toString().length;
 
-for(let  i = 1; i <=9; i++){
-    let /**
-     * @param {number} low
-     * @param {number} high
-     * @return {number[]}
-     */
-    var sequentialDigits = function(low, high) {
-        let result = [];
-        let sample = '123456789';
-        let n = 10;
-
-        let lowLength = low.toString().length;
-        let highLength = high.toString().length;
-
-        for (let j = lowLength; j < highLength + 1; j++) {
-            for (let i = 0; i < n - j; i++) {
-                let num = parseInt(sample.substring(i, i + j));
-                if (num >= low && num <= high) result.push(num);
-            }
-        }
-
-        return result;
-    };num = i;
-    for(let j = i + 1; j <=9; j++){
-        num = num * 10 + j;
-        console.log(num);
-
-        if(num >= low && num <= high){
-            res.push(num);
+    for (let j = lowLength; j < highLength + 1; j++) {
+        for (let i = 0; i < n - j; i++) {
+            let num = parseInt(sample.substring(i, i + j));
+            if (num >= low && num <= high) result.push(num);
         }
     }
-}
-console.log(res.sort((a, b) => a - b))
+
+    return result;
+};
