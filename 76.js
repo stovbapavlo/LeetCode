bdb614e7eb08ee982979cccc635ccff1c3054d03
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-04 14:46:21 +0200

    solution 76

--- 76.js
+++ 76.js
@@ -1,32 +1,40 @@
-let count = Object.keys(map).length;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    let min = "", left = 0, right = -1;
+    let map = {};
 
-while (right <= s.length) {
-    // found a valid substring
-    if (count == 0) {
+    t.split('').forEach(element => {
+        if (map[element]==null) map[element] = 1;
+        else map[element] = map[element] + 1;
+    });
+    let count = Object.keys(map).length;
 
-        // try to shift left boudary to the right, this means the very left character will be removed
-        // because of this, we need to check whats the affect by removing that character,
-        let current = s[left];
+    while (right <= s.length) {
+        if (count == 0) {
+            let current = s[left];
 
-        // if this chacter is in our map, it means we ll need to find another one in the future
-        if (map[current] != null) map[current]++;
+            if (map[current] != null) map[current]++;
 
-        // * we must have the condition `>0` because for case like "BBBA...", count for B could be negative
-        if (map[current] > 0) count++;
+            if (map[current] > 0) count++;
 
-        let temp = s.substring(left, right+1)
-        if (min == "") min = temp;
-        else min = min.length<temp.length?min:temp;
+            let temp = s.substring(left, right+1)
+            if (min == "") min = temp;
+            else min = min.length<temp.length?min:temp;
 
-        left++;
-    } else {
-        right++;
-        let current = s[right];
+            left++;
+        } else {
+            right++;
+            let current = s[right];
 
-        // decrease the count for this character
-        if (map[current] != null) map[current]--;
+            if (map[current] != null) map[current]--;
 
-        if (map[current] == 0) count--;
+            if (map[current] == 0) count--;
+        }
     }
-}
-return min;
+    return min;
+
+};
