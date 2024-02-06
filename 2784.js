dd7ed12973534a3454781be23f9e90ed2d2679a3
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-06 08:47:12 +0200

    solution 2784

--- 2784.js
+++ 2784.js
@@ -4,5 +4,6 @@ nums.sort((a, b) => a - b)
 let n = nums.length, v = 1;
 if(nums[n - 1] !== nums[n - 2]) return false
 for(let i = 0; i < n - 1; i++){
-    if(nums[i] !=- v++) return false;
+    if(nums[i] !== v++) return false;
 }
+return true
