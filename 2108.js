760249d2c052159c9f0cb59aa842a24ea58b8e23
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-13 12:38:31 +0200

    solution 2108

---
+++ 2108.js
@@ -0,0 +1,20 @@
+let words = ["abc","car","sa","raceacar","cool"];
+let res;
+function isPalindromic(text) {
+    let n = text.length;
+    let revers = '';
+    for(let i = n; i > 0; i--){
+        revers += text[i - 1];
+    }
+    return text === revers
+}
+
+for(let i = 0; i < words.length; i++){
+    if(isPalindromic(words[i])){
+        res = words[i]
+        break;
+    }
+}
+
+console.log(res === '' ? '': res)
+
