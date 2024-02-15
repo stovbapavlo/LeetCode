ce1e990be14994affac88c11334da9d3abc94319
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-15 10:49:03 +0200

    solution 2971

--- 2971.js
+++ 2971.js
@@ -1,11 +1,17 @@
-nums.sort((a, b) => a - b)
-let sum = 0;
-let max = -1;
-for(let i = 0; i < nums.length; i++){
-    if(i >= 2 && sum > nums[i]){
-        max = sum + nums[i];
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+    nums.sort((a, b) => a - b)
+    let sum = 0;
+    let max = -1;
+    for(let i = 0; i < nums.length; i++){
+        if(i >= 2 && sum > nums[i]){
+            max = sum + nums[i];
+        }
+        sum += nums[i]
     }
-    sum += nums[i]
-}
 
-console.log(max)
+    return max
+};
