ad5cff2dcfda175af8fcd958b37945e784d5f996
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-23 06:56:24 +0200

    solution 787

--- 787.js
+++ 787.js
@@ -1,10 +1,20 @@
-let M = Array.from({ length: n + 1 }, () => Infinity);
-M[src] = 0;
-for (let k = 0; k < ka + 1; k++) {
-    let N = [...M]
-    for (let [from, to, price] of flights) {
-        N[to] = Math.min(N[to], M[from] + price);
+/**
+ * @param {number} n
+ * @param {number[][]} flights
+ * @param {number} src
+ * @param {number} dst
+ * @param {number} k
+ * @return {number}
+ */
+var findCheapestPrice = function(n, flights, src, dst, k) {
+    let M = Array.from({ length: n + 1 }, () => Infinity);
+    M[src] = 0;
+    for (let i = 0; i < k + 1; i++) {
+        let N = [...M]
+        for (let [from, to, price] of flights) {
+            N[to] = Math.min(N[to], M[from] + price);
+        }
+        M = N
     }
-    M = N
+    return M[dst] != Infinity ? M[dst] : -1
 }
-return M[dst] != Infinity ? M[dst] : -1
