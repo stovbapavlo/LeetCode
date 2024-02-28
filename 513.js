dc10404b3cbe757ffd6e05a8f169719826db11fa
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-28 09:46:21 +0200

    solution 513

---
+++ 513.js
@@ -0,0 +1,17 @@
+const queue = [root];
+let leftmostValue;
+
+while (queue.length > 0) {
+    const node = queue.shift();
+
+    leftmostValue = node.val;
+
+    if (node.right) {
+        queue.push(node.right);
+    }
+    if (node.left) {
+        queue.push(node.left);
+    }
+}
+
+return leftmostValue;
