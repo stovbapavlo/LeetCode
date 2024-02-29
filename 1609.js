4d5475ad44f671b9d6ae984aac4fe698d2f32da7
Stovba Pavlo <stovbapavlo@gmail.com>
2024-02-29 07:07:06 +0200

    solution 1609

---
+++ 1609.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isEvenOddTree = function(root) {
+    let queue = [root];
+    let steps = 0;
+
+    while(queue.length > 0){
+        const currLength = queue.length;
+        let nextQueue = []
+
+        for(let i = 0; i < currLength;  i++){
+            const node = queue[i]
+            if(steps !== 0){
+                if((i<currLength-1 && node.val<=queue[i+1].val) || node.val%2 !== 0){
+                    return false;
+                }
+            }
+            else if((i<currLength-1 && node.val>=queue[i+1].val)|| node.val%2 === 0){
+                return false;
+            }
+            if(node.left) nextQueue.push(node.left)
+            if(node.right) nextQueue.push(node.right)
+        }
+        queue = nextQueue
+        steps++
+    }
+    return true;
+};
