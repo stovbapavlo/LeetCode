9e8f246f4a3538e987a4fc05cb6afb95870c1616
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-04 09:12:13 +0200

    solution 948

---
+++ 948.js
@@ -0,0 +1,21 @@
+tokens.sort((a, b) => a - b)
+
+let score = 0;
+let resPower = power;
+
+while (tokens.length !== 0) {
+    const flipLeft = resPower - tokens[0];
+
+    if (tokens.length === 1 && flipLeft < 0) break;
+
+    if (flipLeft >= 0) {
+        resPower = flipLeft;
+        score++;
+        tokens.shift();
+    }
+    else if (score >= 1) {
+        resPower += tokens.pop();
+        score--;
+    }
+    else break;
+}
