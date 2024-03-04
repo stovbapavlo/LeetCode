28c3e5d9b8c70d18d4fda78ed4ecb6a9d8d2686e
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-04 09:12:27 +0200

    solution 948

--- 948.js
+++ 948.js
@@ -1,21 +1,24 @@
-tokens.sort((a, b) => a - b)
+var bagOfTokensScore = function(tokens, power) {
+    tokens.sort((a, b) => a - b)
 
-let score = 0;
-let resPower = power;
+    let score = 0;
+    let resPower = power;
 
-while (tokens.length !== 0) {
-    const flipLeft = resPower - tokens[0];
+    while (tokens.length !== 0) {
+        const flipLeft = resPower - tokens[0];
 
-    if (tokens.length === 1 && flipLeft < 0) break;
+        if (tokens.length === 1 && flipLeft < 0) break;
 
-    if (flipLeft >= 0) {
-        resPower = flipLeft;
-        score++;
-        tokens.shift();
+        if (flipLeft >= 0) {
+            resPower = flipLeft;
+            score++;
+            tokens.shift();
+        }
+        else if (score >= 1) {
+            resPower += tokens.pop();
+            score--;
+        }
+        else break;
     }
-    else if (score >= 1) {
-        resPower += tokens.pop();
-        score--;
-    }
-    else break;
-}
+    return score
+};
