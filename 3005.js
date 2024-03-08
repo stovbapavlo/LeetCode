b452d95a02f554de90c455d7dd73a8489a4e9f32
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-08 10:05:59 +0200

    solution 3005

--- 3005.js
+++ 3005.js
@@ -1,4 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var maxFrequencyElements = function(nums) {
-    const set = new Set(nums)
-    return set.size
+    let frequenciesMap = {}
+
+    let maxFrequencies = 0;
+
+    for(let num of nums) {
+        frequenciesMap[num] = (frequenciesMap[num] || 0) + 1
+        maxFrequencies = Math.max(maxFrequencies, frequenciesMap[num])
+    }
+
+    let countMaxFrequency = 0;
+    for(let num in frequenciesMap) {
+        if(frequenciesMap[num] === maxFrequencies) {
+            countMaxFrequency++;
+        }
+    }
+    return countMaxFrequency;
 };
