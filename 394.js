36b6775c4c33f77da8ca715437e29f997b927dcf
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-10 11:07:59 +0200

    solution 394

--- 394.js
+++ 394.js
@@ -1,14 +1,19 @@
-let nums1 = [1,2,2,1], nums2 = [2,2]
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const set1 = new Set(nums1)
+    const set2 = new Set(nums2)
 
-const set1 = new Set(nums1)
-const set2 = new Set(nums2)
+    let res = []
 
-let res = []
-
-for(let value of set1) {
-    if(set2.has(value)){
-        res.push(value)
+    for(let value of set1) {
+        if(set2.has(value)){
+            res.push(value)
+        }
     }
-}
 
-console.log(res)
+    return res
+};
