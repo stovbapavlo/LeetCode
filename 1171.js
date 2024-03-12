1a237ecc50e728eec78cb15ce41bfe904bdb4eb6
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-12 10:44:44 +0200

    solution 1171

--- 1171.js
+++ 1171.js
@@ -1,34 +1,19 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var removeZeroSumSublists = function(head) {
-    let prefixSum = 0;
-    let sumMap = new Map();
-    let dummy = new ListNode(0);
-    dummy.next = head;
-
-    let current = dummy;
-    while(current) {
-        prefixSum += current.val;
-        sumMap.set(prefixSum, current)
-        current = current.next;
+const map = new Map();
+let runningSum = 0;
+const dummyHead = new ListNode(0, head);
+let currNode = dummyHead;
+while (currNode !== null) {
+    runningSum += currNode.val;
+    map.set(runningSum, currNode);
+    currNode = currNode.next;
+}
+currNode = dummyHead;
+runningSum = 0;
+while (currNode !== null) {
+    runningSum += currNode.val;
+    if (map.get(runningSum) !== currNode) {
+        currNode.next = map.get(runningSum).next;
     }
-
-    prefixSum = 0;
-    current = dummy;
-    while(current) {
-        prefixSum += current.val;
-        current.next = sumMap.get(prefixSum).next;
-        current = current.next;
-    }
-
-    return dummy.next
-};
+    currNode = currNode.next;
+}
+return dummyHead.next;
