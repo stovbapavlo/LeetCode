97b5fde9d518a4f621d44dd8a7501d89c42fdf94
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-12 10:40:19 +0200

    solution 1171

---
+++ 1171.js
@@ -0,0 +1,24 @@
+const dummy = new ListNode(0);
+dummy.next = head;
+let prefix_sum = 0;
+const prefix_sums = { 0: dummy };
+let current = head;
+
+while (current) {
+    prefix_sum += current.val;
+    if (prefix_sum in prefix_sums) {
+        let to_delete = prefix_sums[prefix_sum].next;
+        let temp_sum = prefix_sum + to_delete.val;
+        while (to_delete !== current) {
+            delete prefix_sums[temp_sum];
+            to_delete = to_delete.next;
+            temp_sum += to_delete.val;
+        }
+        prefix_sums[prefix_sum].next = current.next;
+    } else {
+        prefix_sums[prefix_sum] = current;
+    }
+    current = current.next;
+}
+
+return dummy.next;
