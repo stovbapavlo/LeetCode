466d23f54b8ac4d95cf876a3778bf1eee8326ed1
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-12 10:45:34 +0200

    solution 1171

--- 1171.js
+++ 1171.js
@@ -1,19 +1,32 @@
-const map = new Map();
-let runningSum = 0;
-const dummyHead = new ListNode(0, head);
-let currNode = dummyHead;
-while (currNode !== null) {
-    runningSum += currNode.val;
-    map.set(runningSum, currNode);
-    currNode = currNode.next;
-}
-currNode = dummyHead;
-runningSum = 0;
-while (currNode !== null) {
-    runningSum += currNode.val;
-    if (map.get(runningSum) !== currNode) {
-        currNode.next = map.get(runningSum).next;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeZeroSumSublists = function(head) {
+    const map = new Map();
+    let runningSum = 0;
+    const dummyHead = new ListNode(0, head);
+    let currNode = dummyHead;
+    while (currNode !== null) {
+        runningSum += currNode.val;
+        map.set(runningSum, currNode);
+        currNode = currNode.next;
     }
-    currNode = currNode.next;
-}
-return dummyHead.next;
+    currNode = dummyHead;
+    runningSum = 0;
+    while (currNode !== null) {
+        runningSum += currNode.val;
+        if (map.get(runningSum) !== currNode) {
+            currNode.next = map.get(runningSum).next;
+        }
+        currNode = currNode.next;
+    }
+    return dummyHead.next;
+};
