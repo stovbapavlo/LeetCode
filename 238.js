50d623e7d2e2002fc7c877239585ea2dc3eb6248
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-15 09:18:33 +0200

    solution 238

---
+++ 238.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const prefix = []
+
+    for(let i = 0; i < nums.length; i++) {
+        if(i === 0) {
+            prefix[i] = 1
+        }
+        else {
+            prefix[i] = nums[i - 1] * prefix[i - 1]
+        }
+    }
+
+    const suffix = []
+
+    for(let i = nums.length - 1; i >= 0; i--) {
+        if(i === nums.length - 1) {
+            suffix[i] = 1
+        }
+        else {
+            suffix[i] = nums[i + 1] * suffix[i + 1]
+        }
+    }
+
+    const result = []
+
+    for(let i = 0; i < nums.length; i++) {
+        result[i] = prefix[i] * suffix[i]
+    }
+
+    return result
+};
