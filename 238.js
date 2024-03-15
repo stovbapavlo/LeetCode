92b586409b206f1230fe1f25a84baaee37d8ded1
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-15 09:20:16 +0200

    solution 238

--- 238.js
+++ 238.js
@@ -3,33 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const prefix = []
-
-    for(let i = 0; i < nums.length; i++) {
-        if(i === 0) {
-            prefix[i] = 1
-        }
-        else {
-            prefix[i] = nums[i - 1] * prefix[i - 1]
-        }
+    let res = [1, nums[0]];
+    for(let i = 1; i < nums.length - 1; i++){
+        res.push(res[i] * nums[i])
     }
 
-    const suffix = []
-
-    for(let i = nums.length - 1; i >= 0; i--) {
-        if(i === nums.length - 1) {
-            suffix[i] = 1
-        }
-        else {
-            suffix[i] = nums[i + 1] * suffix[i + 1]
-        }
+    let postFix = 1
+    for(let i = nums.length - 1; i >= 0; i--){
+        res[i] *= postFix;
+        postFix *= nums[i]
     }
 
-    const result = []
-
-    for(let i = 0; i < nums.length; i++) {
-        result[i] = prefix[i] * suffix[i]
-    }
-
-    return result
+    return res;
 };
