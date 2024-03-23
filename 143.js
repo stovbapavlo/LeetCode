4bb22cc752ad40cccebf6999b817909c4ebcd052
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-23 11:13:45 +0200

    solution 143

--- 143.js
+++ 143.js
@@ -1,4 +1,14 @@
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
 const reorderList = function(head) {
     let stack = [];
     let node = head;
@@ -18,8 +28,9 @@ const reorderList = function(head) {
         }
         else {
             node.next = stack.pop();
-            node= node.next
         }
-        node.next = null;
+        node= node.next
+
     }
+    node.next = null;
 }
