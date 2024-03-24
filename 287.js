3f1ef3453281269bca2f2363b264df7b3b1e790a
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-24 10:59:15 +0200

    solution 287

--- 287.js
+++ 287.js
@@ -11,3 +11,12 @@ var findDuplicate = function(nums) {
         }
     }
 };
+
+
+/*for (let num of nums) {
+        let index = Math.abs(num);
+        nums[index] *= -1;
+        if (nums[index] >= 0) {
+            return Math.abs(num);
+        }
+    }*/
