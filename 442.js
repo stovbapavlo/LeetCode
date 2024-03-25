a1b620a9ace40b9f1e6901c0e85a1818765594b9
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-25 09:50:44 +0200

    solution 442

--- 442.js
+++ 442.js
@@ -2,7 +2,16 @@ let nums = [4,3,2,7,8,2,3,1];
 
 const hash = {};
 const duplicates = []
-nums.forEach( num => {
+
+
+nums.forEach(num => {
+    hash[num] = (hash[num] || 0) + 1;
+})
+
+duplicates.push(...Object.keys(hash).filter(key => hash[key] > 1).map(Number));
+
+
+/*nums.forEach( num => {
     if(hash[num]) {
         hash[num]++
     }
@@ -15,6 +24,6 @@ for(const key in hash) {
     if(hash[key] > 1) {
         duplicates.push(parseInt(key))
     }
-}
+}*/
 
 console.log(duplicates)
