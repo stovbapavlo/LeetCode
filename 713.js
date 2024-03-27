c03cfe4b30c5d202e762444acdaad4fa92063561
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-27 08:25:07 +0200

    solution 713

--- 713.js
+++ 713.js
@@ -1,16 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
 var numSubarrayProductLessThanK = function(nums, k) {
-    let n = nums.length;
+    if(k <= 1) return 0;
 
-    let count  = 0;
-    for(let i = 0; i < n; i++) {
-        let prod = 1;
-        for(let j = i; j < n; j++) {
-            prod *= nums[j]
-            if(prod >= k) {
-                break;
-            }
-            count++
+    let i=0, count =0, prod=1
+
+    for(let j=0; j < nums.length; j++){
+
+        prod *= nums[j];
+
+        while(prod >= k && i <= j){
+            prod /= nums[i];
+            i++;
         }
+
+        count += j-i+1;
     }
+
     return count;
 };
