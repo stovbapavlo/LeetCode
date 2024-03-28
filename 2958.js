3dfa9b5a126a7a1cc3bec13028fd8f0cf3010cb0
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-28 10:18:43 +0200

    solution 2958

---
+++ 2958.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubarrayLength = function(nums, k) {
+    let left = 0;
+    let right = 0;
+    let maxLen = 0;
+    const dict = {};
+
+    while (right < nums.length) {
+        dict[nums[right]] = (dict[nums[right]] ?? 0) + 1;
+
+        while (dict[nums[right]] > k && left <= right) {
+            dict[nums[left]] = dict[nums[left]] - 1;
+            left++;
+        }
+
+        maxLen = Math.max(maxLen, right - left + 1)
+        right++;
+    }
+
+    return maxLen;
+};
