785004b640fd55db7b632c5dc3abc7723ca3dd8a
Stovba Pavlo <stovbapavlo@gmail.com>
2024-03-29 09:02:59 +0200

    solution 2962

--- 2962.js
+++ 2962.js
@@ -1,13 +1,13 @@
 
-var countSubarrays = function(nums, k) {
-    let subarrays = 0, count = 0;
+var countSubArrays = function(nums, k) {
+    let subArrays = 0, count = 0;
     const max = Math.max(...nums);
     for (let l = 0, r = 0; r < nums.length; r++) {
         if (nums[r] === max) count++;
         while (count >= k) {
-            subarrays += nums.length - r;
+            subArrays += nums.length - r;
             if (nums[l++] === max) count--;
         }
     }
-    return subarrays;
+    return subArrays;
 }
