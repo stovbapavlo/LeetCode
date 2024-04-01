0a63363334ee8ddaa17f4c1344d01d34b345f3f1
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-01 09:38:17 +0300

    solution 58

--- 58.js
+++ 58.js
@@ -7,3 +7,11 @@ var lengthOfLastWord = function(s) {
 
     return parts[parts.length - 1].length;
 };
+
+///**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var lengthOfLastWord = function(s) {
+//     return s.trimEnd().split(' ').at(-1).length
+// };
