078ad8008a7815fde1a8690546d439331769c820
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-04 10:08:07 +0300

    solution 1614

--- 1614.js
+++ 1614.js
@@ -1,13 +1,17 @@
 const s = "(1+(2*3)+((8)/4))+1";
 
-let counter = 0;
-let n = s.length;
-let maxCounter = 0;
+var maxDepth = function(s) {
+    let counter = 0;
+    let n = s.length;
+    let maxCounter = 0;
 
-for(let i = 0; i < n; i++){
-    if(s[i] === "(") counter++
-    else if (counter > maxCounter) maxCounter = counter
-    else if(s[i] === ")") counter--
-}
+    for(let i = 0; i < n; i++){
+        if(s[i] === "(") {
+            counter++
+            maxCounter = Math.max(counter, maxCounter)
+        }
+        else if(s[i] === ")") counter--
+    }
+    return maxCounter
+};
 
-console.log(maxCounter)
