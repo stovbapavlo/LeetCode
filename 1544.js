744e2840dc631f41ca4753ee9343b220af5565dd
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-05 09:51:00 +0300

    solution 1544

---
+++ 1544.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function(s) {
+    const stack = [''];
+
+    for(let c of s) {
+        let top = stack[stack.length-1];
+        if(top.toLowerCase() === c.toLowerCase() && top !== c) stack.pop()
+        else stack.push(c);
+    }
+    return stack.join('');
+
+};
