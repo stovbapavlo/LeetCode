d1b6736df3f4c9f03810c790f46d0179b5eb7ce5
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-06 09:30:16 +0300

    solution 1249

--- 1249.js
+++ 1249.js
@@ -1,47 +1,18 @@
-function isEquals(str1 , str2) {
-    return str1 === str2;
-
-}
-
-function isUnique(str) {
-    let set = new Set(str);
-    return set.size === str.length;
-}
-
-function generateSubsequences(arr, current, index){
-    if(index === arr.length) {
-
-        if(isUnique(current) && current.length > maxLength) {
-            maxLength = current.length
-        }
-        return maxLength;
-    }
-    generateSubsequences(arr, current, index + 1);
-
-    generateSubsequences(arr, current+ arr[index], index + 1);
-
-    return maxLength;
-}
-let arr = ["un","iq","ue"];
-
-
-/*
-
-/!**
- * @param {string[]} arr
- * @return {number}
- *!/
-var maxLength = function(arr) {
-    let res = 0;
-    function solve(index, cur) {
-        if(cur.length !== new Set(cur.split('')).size) {
-            return;
-        }
-        res = Math.max(res, cur.length);
-        for(let i = index; i < arr.length; i++) {
-            solve(i + 1, `${cur}${arr[i]}`);
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function(s) {
+    s = s.split("");
+    let stack = [];
+    for(let i = 0; i < s.length; i++){
+        if (s[i] === "(") stack.push(i);
+        else if (s[i] === ")") {
+            if(stack.length) stack.pop();
+            else s[i] = ""
         }
     }
-    solve(0, '');
-    return res;
-};*/
+    for(let i of stack) s[i] = "";
+
+    return s.join("")
+};
