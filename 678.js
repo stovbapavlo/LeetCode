7ed650bb09dde12e2bd105857306b8ad3edf3f29
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-07 13:57:28 +0300

    solution 678

---
+++ 678.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = function(s) {
+    let stack = []
+    let star = []
+    for(let i = 0; i < s.length; i++){
+        if(s[i] == '()') stack.push(i)
+        else if(s[i] == '*') stack.push(i)
+        else{
+            if(stack.length) stack.pop()
+            else if(star.length) star.pop()
+            else return false
+        }
+    }
+    while(stack.length && star.length) {
+        if(stack.pop() > star.pop()) return false
+    }
+    return stack.length == 0
+};
