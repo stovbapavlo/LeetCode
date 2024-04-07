654a8ead4f576e42c3815791ce6d670a1183d8da
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-07 14:00:27 +0300

    solution 678

--- 678.js
+++ 678.js
@@ -3,20 +3,30 @@
  * @return {boolean}
  */
 var checkValidString = function(s) {
-    let stack=[]
-    let star=[]
-    for(let i=0;i<s.length;i++){
-        if(s[i]=='(') stack.push(i)
-        else if(s[i]=='*') star.push(i)
-        else{
-            if(stack.length) stack.pop()
-            else if(star.length) star.pop()
-            else return false
+    let openStack = [];
+    let asteriskStack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            openStack.push(i);
+        } else if (s[i] === '*') {
+            asteriskStack.push(i);
+        } else {
+            if (openStack.length > 0) {
+                openStack.pop();
+            } else if (asteriskStack.length > 0) {
+                asteriskStack.pop();
+            } else {
+                return false;
+            }
         }
     }
 
-    while(stack.length&&star.length){
-        if(stack.pop()>star.pop()) return false
+    while (openStack.length > 0 && asteriskStack.length > 0) {
+        if (openStack.pop() > asteriskStack.pop()) {
+            return false;
+        }
     }
-    return stack.length==0
+
+    return openStack.length === 0;
 };
