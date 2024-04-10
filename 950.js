0842ed4306a421b69ec88d441a2f056719cfe030
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-10 09:34:55 +0300

    solution 950

---
+++ 950.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} deck
+ * @return {number[]}
+ */
+var deckRevealedIncreasing = function(deck) {
+    let stack = deck.sort((a, b) => b - a)
+
+    let queue = [stack.shift()]
+
+    while (stack.length > 0) {
+        queue.unshift(queue.pop());
+        queue.unshift(stack.shift())
+    }
+
+    return queue
+
+};
