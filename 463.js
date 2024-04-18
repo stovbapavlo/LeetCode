209e2efb0753559b9448dfffd992d687deaf8993
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-18 08:53:58 +0300

    solution 463

--- 463.js
+++ 463.js
@@ -1,16 +1,22 @@
 const grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
 
+const rows = grid.length;
+const cols = grid[0].length;
+
 let perimeter = 0;
 
-for(let row = 0; row < grid.length; row++) {
-    for(let col = 0; col < grid[0].length; col++) {
-        if(grid[row][col] === 1) {
-            if (row < grid.length - 1 && grid[row + 1][col] !== 1) perimeter++;
-            if (row > 0 && grid[row - 1][col] !== 1) perimeter++;
-            if (col < grid[0].length - 1 && grid[row][col + 1] !== 1) perimeter++;
-            if (col > 0 && grid[row][col - 1] !== 1) perimeter++;
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+        if (grid[row][col] === 1) {
+            perimeter += 4;
+
+            if (row > 0 && grid[row - 1][col] === 1) perimeter--;
+            if (row < rows - 1 && grid[row + 1][col] === 1) perimeter--;
+            if (col > 0 && grid[row][col - 1] === 1) perimeter--;
+            if (col < cols - 1 && grid[row][col + 1] === 1) perimeter--;
         }
     }
 }
+
 console.log(perimeter);
 
