996d8d84ea4fcbfc562a403f7b2a187aca239dd2
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-23 09:46:06 +0300

    solution 310

--- 310.js
+++ 310.js
@@ -4,50 +4,32 @@
  * @return {number[]}
  */
 var findMinHeightTrees = function(n, edges) {
-    if (n === 1) {
-        return [0]
+    if (n === 1) return [0]
+    const adjList = new Array(n).fill(0).map(() => [])
+    const degrees = new Array(n).fill(0)
+    for (const [a, b] of edges) {
+        adjList[a].push(b)
+        adjList[b].push(a)
+        degrees[a]++
+        degrees[b]++
     }
 
-    const indegrees = new Array(n).fill(0)
-    const graph = new Map()
-
+    let leaves = []
     for (let i = 0; i < n; i++) {
-        graph.set(i, [])
-    }
-
-    for (const [e1, e2] of edges) {
-        graph.get(e1).push(e2)
-        graph.get(e2).push(e1)
-
-        indegrees[e1] += 1
-        indegrees[e2] += 1
-    }
-
-    const queue = []
-
-    for (let i = 0; i < n; i++) {
-        if (indegrees[i] === 1) {
-            queue.push(i)
-        }
+        if (degrees[i] === 1) leaves.push(i)
     }
 
     while (n > 2) {
-        let size = queue.length
-        n -= size
-
-        while (size > 0) {
-            const node = queue.shift()
-            size -= 1
-
-            for (const neighbour of graph.get(node)) {
-                indegrees[neighbour] -= 1
-
-                if (indegrees[neighbour] === 1) {
-                    queue.push(neighbour)
-                }
+        n -= leaves.length
+        let newLeaves = []
+        for (let i of leaves) {
+            for (let j of adjList[i]) {
+                degrees[j]--
+                if (degrees[j] === 1) newLeaves.push(j)
             }
         }
+        leaves = newLeaves
     }
 
-    return queue
+    return leaves
 };
