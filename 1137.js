53f288e80562e53318c151bc7175be66bc525c38
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-24 09:23:26 +0300

    solution 1137

--- 1137.js
+++ 1137.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var tribonacci = function(n) {
-    let result = [0,1,1]
-
-    for(let i = 3; i<=n; i++) {
-        result[i] = result[i-1] + result[i-2] + result[i-3]
+    let arr = new Array(n+1);
+    arr[0] = 0;
+    arr[1] = 1;
+    arr[2] = 1;
+    for(let i=3; i<arr.length; i++) {
+        arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
     }
-
-    return result[n]
+    return arr[n];
 };
