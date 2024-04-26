85f58e7248008cba5f03c2a977d15f16dafef304
Stovba Pavlo <stovbapavlo@gmail.com>
2024-04-26 12:00:07 +0300

    solution 67

--- 67.js
+++ 67.js
@@ -4,21 +4,5 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    a = a.split("").reverse().join("");
-    b = b.split("").reverse().join("");
-    len = a.length > b.length ? a.length : b.length;
-    result = [];
-    for(let i = 0; i < len; i += 1){
-        num1 = Number(a[i] || 0);
-        num2 = Number(b[i]) || 0;
-        curr = Number(result[i]||0) + num1 + num2
-        if(curr >= 2){
-            result[i] = curr%2;
-            result.push(1)
-        }
-        else{
-            result[i] = curr
-        }
-    }
-    return result.reverse().join("")
+    return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2);
 };
