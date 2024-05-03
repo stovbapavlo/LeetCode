46f27671e05859c2cbb6a98fcac4c3d216fea04c
Stovba Pavlo <stovbapavlo@gmail.com>
2024-05-03 23:37:20 +0300

    solution 165

---
+++ 165.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    version1 = version1.split('.');
+    version2 = version2.split('.');
+
+    let index = 0;
+
+    while (index < version1.length || index < version2.length) {
+        const num1 = version1[index] ? parseInt(version1[index]) : 0;
+        const num2 = version2[index] ? parseInt(version2[index]) : 0;
+
+        if (num1 < num2) {
+            return -1;
+        } else if (num1 > num2) {
+            return 1;
+        } else {
+            index++;
+        }
+    }
+
+    return 0;
+
+};
