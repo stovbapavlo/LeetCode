aa9774e81292a915e9b32e816bf10cfbbe63eb2d
Stovba Pavlo <stovbapavlo@gmail.com>
2024-05-24 12:27:27 +0300

    solution 1863

--- 1863.js
+++ 1863.js
@@ -3,19 +3,14 @@
  * @return {number}
  */
 var subsetXORSum = function(nums) {
-    const subsets = [[]]
-    let sum = 0
-    for(const el of nums){
-        const last = subsets.length-1
-        for(let i = 0; i<=last; i++){
-            subsets.push([...subsets[i],el])
-        }
-    }
+    let sum=0;
+    dfs(0, 0); return sum;
 
-    for(let j = 0; j<subsets.length;j++){
-        if (subsets[j].length === 0) sum = sum +0
-        else if(subsets[j].length === 1) sum = sum + parseInt(subsets[j])
-        else sum = sum + parseInt(subsets[j].reduce((acc,curr) => acc^curr))
+    function dfs(val, i){
+        if(i<nums.length){
+            dfs(val^nums[i], i+1);
+            dfs(val, i+1);
+        }
+        if(i==nums.length){sum+=val};
     }
-    return sum
 };
