ec52ebc0fcb3bb43f5e75ca6107e9acd33939d92
Stovba Pavlo <stovbapavlo@gmail.com>
2024-05-26 09:53:05 +0300

    solution 367

--- 367.js
+++ 367.js
@@ -3,5 +3,23 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    return Number.isInteger(Math.sqrt(num));
+    if (num === 1)
+        return true;
+
+    let l = 1;
+    let r = Math.floor(num / 2);
+
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        let mSqr = m * m;
+
+        if (mSqr === num)
+            return true;
+        else if (mSqr < num)
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+
+    return false;
 };
