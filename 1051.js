896070e6f42f8d69753005e54abf9f30c0a21c68
Stovba Pavlo <stovbapavlo@gmail.com>
2024-06-10 10:24:27 +0300

    solution 1051

--- 1051.js
+++ 1051.js
@@ -3,12 +3,14 @@
  * @return {number}
  */
 var heightChecker = function(heights) {
-    const sortedHeights = [...heights].sort((a,b) => a - b);
-    let output = 0;
-
-    for (let i = 0; i < heights.length; i++) {
-        if (heights[i] !== sortedHeights[i]) output++;
+    let num = 0;
+    let copy = [...heights].sort((a,b) => a-b)
+    for (let i=0; i<heights.length; i++) {
+        if (copy[i] != heights[i]) {
+            num++
+        }
     }
 
-    return output;
+
+    return num;
 };
