d8b817f4715533b88d8e888efb5b2da4ebb6ba08
Stovba Pavlo <stovbapavlo@gmail.com>
2024-06-14 11:22:42 +0300

    solution 945

--- 945.js
+++ 945.js
@@ -3,16 +3,19 @@
  * @return {number}
  */
 var minIncrementForUnique = function(nums) {
-    nums.sort((a, b) => a - b);
-
-    let numTracker = 0;
-    let minIncrement = 0;
-
-    for (let num of nums) {
-        numTracker = Math.max(numTracker, num);
-        minIncrement += numTracker - num;
-        numTracker++;
+    const max = Math.max(...nums);
+    const bucket = new Array(max + 1).fill(0);
+    for (let i = 0; i < nums.length; i++) {
+        bucket[nums[i]]++;
     }
-
-    return minIncrement;
+    let moves = 0;
+    for (let cur = 0, next = 0; cur <= max; cur++) {
+        while (bucket[cur]-- > 1) {
+            if (cur > next) next = cur + 1;
+            while (next <= max && bucket[next]) next++;
+            moves += next - cur;
+            next <= max ? bucket[next]++ : next++;
+        }
+    }
+    return moves;
 };
