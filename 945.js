a6d29dd41c888d2b97f35bf1fed3429bb881295e
Stovba Pavlo <stovbapavlo@gmail.com>
2024-06-14 11:21:55 +0300

    solution 945

---
+++ 945.js
@@ -0,0 +1,15 @@
+const max = Math.max(...nums);
+const bucket = new Array(max + 1).fill(0);
+for (let i = 0; i < nums.length; i++) {
+    bucket[nums[i]]++;
+}
+let moves = 0;
+for (let cur = 0, next = 0; cur <= max; cur++) {
+    while (bucket[cur]-- > 1) {
+        if (cur > next) next = cur + 1;
+        while (next <= max && bucket[next]) next++;
+        moves += next - cur;
+        next <= max ? bucket[next]++ : next++;
+    }
+}
+return moves;
