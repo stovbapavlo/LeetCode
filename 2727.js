ae8a7e3c67d28a6b6381ca13c3b8e8baad3bc94c
Stovba Pavlo <stovbapavlo@gmail.com>
2024-07-02 13:25:37 +0300

    solution 2727

--- 2727.js
+++ 2727.js
@@ -2,7 +2,7 @@
  * @param {Object|Array} obj
  * @return {boolean}
  */
-const isEmpty = function(obj) {
+const isEmpty = function (obj, nums2) {
     for (let key in obj) {
         return false;
     }
