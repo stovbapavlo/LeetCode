8d66ae0eb721abf8d885e6fb9edcc64a3ad96d9e
Stovba Pavlo <stovbapavlo@gmail.com>
2024-07-03 12:42:46 +0300

    solution 1509

---
+++ 1509.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minDifference = function(nums) {
+    if (nums.length <= 4) return 0
+
+    nums.sort((a, b) => a - b)
+
+    let opts = [
+        nums[nums.length - 4] - nums[0],
+        nums[nums.length - 3] - nums[1],
+        nums[nums.length - 2] - nums[2],
+        nums[nums.length - 1] - nums[3],
+    ]
+
+    return Math.min(...opts)
+};
