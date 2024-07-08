94b43473a37c0d0cd7c08d187be7c1263d107ab3
Stovba Pavlo <stovbapavlo@gmail.com>
2024-07-08 10:51:48 +0300

    solution 1823

--- 1823.js
+++ 1823.js
@@ -4,18 +4,6 @@
  * @return {number}
  */
 var findTheWinner = function(n, k) {
-    let que = []
-    for(let i = 1; i <= n; i++) {
-        que.push(i)
-    }
-
-    while (que.length > 1) {
-        let deleteCount = k - 1
-        while (deleteCount > 0) {
-            que.push(que.shift())
-            deleteCount--
-        }
-        que.shift()
-    }
-    return que.shift()
+    if (n === 1) return 1;
+    return (findTheWinner(n - 1, k) + k - 1) % n + 1;
 };
