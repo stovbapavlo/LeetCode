9275a4b7f0809a13e69e53d1a3828e32612eee8a
Stovba Pavlo <stovbapavlo@gmail.com>
2024-07-10 11:41:37 +0300

    solution 1598

--- 1598.js
+++ 1598.js
@@ -3,14 +3,15 @@
  * @return {number}
  */
 var minOperations = function(logs) {
-    let step = 0;
-    for (let i = 0; i < logs.length; i++) {
-        const log = logs[i];
-        if (log === '../') {
-            if (step > 0) step--;
-        } else if (log !== './') {
-            step++;
+    let count = 0;
+    for(let i = 0; i < logs.length; i++) {
+        if(logs[i] === "../") {
+            if(count > 0) {
+                count--;
+            }
+        } else if(logs[i] !== "./") {
+            count++;
         }
     }
-    return step;
+    return count;
 };
