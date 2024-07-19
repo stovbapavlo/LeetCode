41d60c45761db5149229b82ad87d1c3a922bacda
Stovba Pavlo <stovbapavlo@gmail.com>
2024-07-19 12:43:50 +0300

    solution 1380

--- 1380.js
+++ 1380.js
@@ -1,8 +1,14 @@
-for(let i = 0;i < matrix.length;i++) {
-    let min = Math.min(...matrix[i]),
-        idx = matrix[i].indexOf(min);
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var luckyNumbers  = function(matrix) {
+    for(let i = 0;i < matrix.length;i++) {
+        let min = Math.min(...matrix[i]),
+            idx = matrix[i].indexOf(min);
 
-    if(matrix.every(arr => arr[idx] <= min)) return [min];
-}
+        if(matrix.every(arr => arr[idx] <= min)) return [min];
+    }
 
-return [];
+    return [];
+};
