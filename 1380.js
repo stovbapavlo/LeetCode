a3972ad421288fd96ef9b6aaa8ae9391f8c78bac
Stovba Pavlo <stovbapavlo@gmail.com>
2024-07-19 12:43:33 +0300

    solution 1380

---
+++ 1380.js
@@ -0,0 +1,8 @@
+for(let i = 0;i < matrix.length;i++) {
+    let min = Math.min(...matrix[i]),
+        idx = matrix[i].indexOf(min);
+
+    if(matrix.every(arr => arr[idx] <= min)) return [min];
+}
+
+return [];
