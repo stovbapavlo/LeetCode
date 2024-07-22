8a314c37e7d350c81f7c2c2fc5ed48da6dd8c12d
Stovba Pavlo <stovbapavlo@gmail.com>
2024-07-22 12:07:19 +0300

    solution 2481

--- 2481.js
+++ 2481.js
@@ -19,3 +19,14 @@ var sortPeople = function(names, heights) {
 
     return names
 };
+
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+const sortPeople = (names, heights) =>
+    names
+        .map((c, i) => [c, heights[i]])
+        .sort((a, b) => b[1] - a[1])
+        .map((c) => c[0]);
