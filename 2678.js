5778f72f8f92fd5693592bc6dfac819813fc983f
Stovba Pavlo <stovbapavlo@gmail.com>
2024-08-01 20:58:30 +0300

    solution 2678

--- 2678.js
+++ 2678.js
@@ -1,10 +1,24 @@
 let details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
-
+/*
 let res = details.map(item => {
     let num = parseInt(item.substring(11, 13))
     if (num > 60) {
         return item
     }
-});
+});*/
 
-console.log(res)
+/*let res = details.filter(item => {
+    let number = parseInt(item.substring(11, 13))
+    return number > 60
+})*/
+
+/**
+ * @param {string[]} details
+ * @return {number}
+ */
+var countSeniors = function(details) {
+    return details.filter(item => {
+        let number = parseInt(item.substring(11, 13))
+        return number > 60
+    }).length;
+};
