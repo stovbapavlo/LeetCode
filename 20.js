e7c37251700f89aab745c0b9c2d7570f7297fea8
Stovba Pavlo <stovbapavlo@gmail.com>
2024-08-06 12:21:06 +0300

    solution 20

---
+++ 20.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = []
+    const complement = {
+        ')':'(',
+        '}':'{',
+        ']':'[',
+    }
+
+    for(let char of s) {
+        if (!complement[char]) stack.push(char);
+        else if (stack.pop() != complement[char]) return false;
+    }
+    return stack.length === 0;
+
+};
